c09f4e831b54cdce9ecd574c841a63abdf255b91
PotHead <potHead>
2017-11-15 20:49:57 -0600

    'toHeroku'

--- server.js
+++ server.js
@@ -6,17 +6,25 @@ var index = require('./routes/index');
 var listas = require('./routes/listas');
 var productos = require('./routes/productos');
 
-var port = 3000;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
 //View Engine
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+
+//app.engine('html', require('ejs').renderFile);
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
 
 // Body Parser MW
 app.use(bodyParser.json());
